90f0c661a1fd66e42fb08f479c71ffb0eb93f76f
Tyro951 <Adam.bakir@edu.devinci.fr>
2024-02-25 21:26:19 +0100

    LOL theme training nodeJs

--- back/controllers/HelloController.js
+++
@@ -1,9 +0,0 @@
-const getHello = (req, res) => {
-    res.send ("Hello There!")
-  }
-  
-const getFrenchHello = (req, res) => {
-    res.send ("Bonjour!")
-  }
-
-  export { getHello, getFrenchHello}
